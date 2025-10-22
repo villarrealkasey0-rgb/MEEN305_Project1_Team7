bde882d8332260b638e1948f07e89c2ca22fd560
231vnguyen <81172229+231vnguyen@users.noreply.github.com>
2025-10-21 20:09:52 -0500

    Added sympy

--- code/vince.py
+++ code/vince.py
@@ -1,1 +1,11 @@
-print("hello suraj")
+import numpy as np
+import sympy as sp
+
+##Von mises for Rectangular Cross Section
+#Disregard: Nx, My, Tx
+#von mises = [My*Cz / Iy]
+
+x = sp.symbols('x')
+equation = sp.Eq(2*x+3,7)
+solution = sp.solve(equation, x)
+print(solution)
