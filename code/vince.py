9b29425781ee72ced0748d502d339d0474c94fdd
231vnguyen <81172229+231vnguyen@users.noreply.github.com>
2025-10-21 20:33:14 -0500

    added plot test

--- code/vince.py
+++ code/vince.py
@@ -1,1 +1,12 @@
+import numpy as np
+import sympy as sp
+from sympy.plotting import (plot,plot_parametric)
 
+##Von mises for Rectangular Cross Section
+#Disregard: Nx, My, Tx
+#von mises = [My*Cz / Iy]
+
+x = sp.symbols('x')
+b_x = 8 *x**2
+
+plot(b_x, (x, 0, 5), title="b(x) = 8x^2", xlabel="x", ylabel="b(x)")
