5af0ce54b550525ddf1f3753e0f2583f12114956
Villarrealkasey0-rgb <villarrealkasey0@gmail.com>
2025-10-21 19:38:59 -0500

    add

--- code/main_beam_program.py
+++ code/main_beam_program.py
@@ -2,12 +2,25 @@
 from subfunctions import beam_analysis
 from plot_functions import plot_shear_moment
 
-print("=== MEEN 305 Beam Analysis Program ===")
+def main():
+    print("=== MEEN 305 Beam Analysis Program ===")
 
-# User inputs
-L = float(input("Enter span length (in): "))
-P = float(input("Enter load (lb): "))
-a = float(input("Enter distance from support A (in): "))
+    # User inputs
+    L = float(input("Enter span length (in): "))
+    P = float(input("Enter load (lb): "))
+    a = float(input("Enter distance from support A (in): "))
 
-# Run the beam analy5sis
-beam_analysis(L, P, a)
+    # Run the beam analysis
+    x, V, M, RA, RB = beam_analysis(L, P, a)
+
+    # Print reactions
+    print(f'\nReactions:')
+    print(f"  RA = {RA:.3f} lb")
+    print(f"  RB = {RB:.3f} lb")
+
+    # Plot shear & moment diagrams 
+    plot_shear_moment(x,V,M,a)
+
+if __name__ == "__main__":
+    main()
+    
